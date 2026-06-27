dcc64ad5f8c07fbcb82b42c13c6ff145e27e6bf4
fanchat <407656141@qq.com>
2026-06-27 11:55:40 +0800

    v0.2.3: 商城性能优化 & 导航栏z-index修复
    - 商品图片从 AWS S3 欧洲换成本地山水图
    - Next.js dev → 生产模式，页面秒开不编译
    - 底部导航 z-10 → z-[60]，滚动不被内容遮挡
    - 商城页面响应速度大幅提升

--- storefront/src/app/[countryCode]/social/layout.tsx
+++ storefront/src/app/[countryCode]/social/layout.tsx
@@ -55,7 +55,7 @@ export default function SocialLayout({
       <div className="flex-1 pb-16">{children}</div>
 
       {/* Bottom nav */}
-      <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200">
+      <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200">
         <div className="flex justify-around items-center h-14 max-w-lg mx-auto">
           {tabs.map((tab) => (
             <Link
--- storefront/src/modules/layout/components/store-bottom-nav/index.tsx
+++ storefront/src/modules/layout/components/store-bottom-nav/index.tsx
@@ -37,7 +37,7 @@ export default function StoreBottomNav() {
   ]
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t border-gray-200">
+    <nav className="fixed bottom-0 left-0 right-0 z-[60] bg-white border-t border-gray-200">
       <div className="flex justify-around items-center h-14 max-w-lg mx-auto">
         {tabs.map((tab) => (
           <Link
